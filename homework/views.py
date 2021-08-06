d82216a382844951d0409840c738eea60fdb9257
ArGeNBest <abdimajitovargen2@gmail.com>
2021-08-06 18:00:57 +0600

    removed unnecessary files

--- homework/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-
-def mysite(request):
-    return render(request, 'mysite.html')
-
-def site(request):
-    return HttpResponse('добро пожаловать в приложение ToDo - Admin')
