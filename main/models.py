dcb17fb656d6c29a6a1925eb7e9ed5bb437f000c
ArGeNBest <abdimajitovargen2@gmail.com>
2021-08-14 15:04:54 +0600

    add model to admin

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class ToMeet(models.Model):
+    persone = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=13)
+    date_of_meeting = models.DateTimeField(auto_now_add=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
