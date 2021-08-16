2b5bf5b57c497fdee17f75b3e5c94d5177f1183c
ArGeNBest <abdimajitovargen2@gmail.com>
2021-08-16 17:42:18 +0600

    update

--- main/views.py
+++ main/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import ToDo, ToMeet
-
+from .forms import TodoForm
 # Create your views here.
 
 def homepage(request):
     todo_list = ToDo.objects.all()
-    return render(request, 'index.html', {'todo_list': todo_list})
+    context = {'todo_list': todo_list}
+    return render(request, 'index.html', context)
 
 def test(request):
     return render(request, 'test.html')
@@ -14,3 +15,11 @@ def meeting(request):
     meet = ToMeet.objects.all()
     return render(request, 'meeting.html', {'tomeet': meet})
 
+
+def add_todo(request):
+    form = TodoForm()
+    form = TodoForm(request.POST)
+    if form.is_valid():
+        form.save()
+    context = {'todo_list': form}
+    return redirect(homepage)
