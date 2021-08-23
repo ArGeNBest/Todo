a8de3369fa31dd098c032189fdf395fc7547010d
ArGeNBest <abdimajitovargen2@gmail.com>
2021-08-23 20:09:35 +0600

    added new model

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
 
@@ -14,3 +15,8 @@ class ToDo(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
+
+class Habits(models.Model):
+    done_for_today = models.DateTimeField(auto_now_add=True)
+    important = models.BooleanField(default=False)
+    comment = models.TextField()
