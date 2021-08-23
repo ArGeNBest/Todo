d23b386cae09d28acb44e9606ae83bd32ebcc5c2
ArGeNBest <abdimajitovargen2@gmail.com>
2021-08-23 20:09:10 +0600

    migrate habist

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import ToMeet, ToDo
+from .models import ToMeet, ToDo, Habits
 
 # Register your models here.
 admin.site.register(ToMeet)
 admin.site.register(ToDo)
+admin.site.register(Habits)
