2a4426c20bf2ec75b21cae6374365b207d8ee54f
ArGeNBest <abdimajitovargen2@gmail.com>
2021-08-23 20:10:55 +0600

    Interacting with the form

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from typing import Text
 from django.shortcuts import render, HttpResponse, redirect
-from .models import ToDo, ToMeet
+from .models import ToDo, ToMeet, Habits
 # Create your views here.
 
 def homepage(request):
@@ -8,17 +8,39 @@ def homepage(request):
     context = {'todo_list': todo_list}
     return render(request, 'index.html', context)
 
+
 def test(request):
     return render(request, 'test.html')
+
 
 def meeting(request):
     meet = ToMeet.objects.all()
     return render(request, 'meeting.html', {'tomeet': meet})
 
 
-def add_todo(request):
+def add_todo_list(request):
     form = request.POST
     text = form['todo_text']
     todo = ToDo(text=text)
     todo.save()
     return redirect(homepage)
+
+
+def add_for_meeting(request):
+    form = request.POST
+    data = ToMeet(person=form['data_text'], phone_number=form['data_tel'])
+    data.save()
+    return redirect(meeting)
+
+
+def habits(request):
+    habit = Habits.objects.all()
+    context = {'habit': habit}
+    return render(request, 'habits.html', context)
+
+
+def add_habit(request):
+    form = request.POST
+    data = Habits(comment=form['comment'], important=form['important'])
+    data.save()
+    return redirect(habits)
